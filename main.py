e83abdc213363350118859c231ce9acdc87bdf7c
Herivelto Junior <herijr@gmail.com>
2019-05-15 13:58:47 -0300

    adicionados sprites de jogador e chefao

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 
-LARGURA = 360
+LARGURA = 640
 ALTURA = 480
 FPS = 30
 
 # Definir cores
 PRETO = (0, 0, 0)
+AMARELO = (255, 255, 0)
+AZUL = (0, 0, 255)
+VERDE = (0, 255, 0)
 
 # Inicializa o modulo pygame e cria a tela do jogo
 pygame.init()
@@ -14,7 +17,52 @@ tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Meu jogo")
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(AZUL)
+        self.rect = self.image.get_rect()
+        self.rect.top = 10
+        self.rect.left = 10
+        self.speedy = 0
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        self.speedx = 9
+        if keystate[pygame.K_UP]:
+            self.speedy = -9
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 9
+        self.rect.y += self.speedy
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > ALTURA:
+            self.rect.bottom = ALTURA
+
+class Boss(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(AMARELO)
+        self.rect = self.image.get_rect()
+        self.rect.centery = ALTURA / 2
+        self.rect.right = LARGURA - 10
+        self.veloc_y = 7
+    def update(self):
+        self.rect.y += self.veloc_y
+        if self.rect.bottom > ALTURA:
+            self.veloc_y = -9
+        if self.rect.top < 0:
+            self.veloc_y = 9
+
 game_sprites = pygame.sprite.Group()
+player = Player()
+boss = Boss()
+game_sprites.add(player)
+game_sprites.add(boss)
 
 # Game Loop
 rodando = True
