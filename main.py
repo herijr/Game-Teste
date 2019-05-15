d659bc405eec35a834640118c38cc4960402cb82
Herivelto Junior <herijr@gmail.com>
2019-05-15 17:16:24 -0300

    configurados os tiros

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ PRETO = (0, 0, 0)
 AMARELO = (255, 255, 0)
 AZUL = (0, 0, 255)
 VERDE = (0, 255, 0)
+VERMELHO = (255, 0, 0)
 
 # Inicializa o modulo pygame e cria a tela do jogo
 pygame.init()
@@ -17,7 +18,7 @@ tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Meu jogo")
 clock = pygame.time.Clock()
 
-class Player(pygame.sprite.Sprite):
+class Jogador(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 40))
@@ -25,22 +26,32 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.top = 10
         self.rect.left = 10
-        self.speedy = 0
+        self.veloc_y = 0
+        self.tiro_intervalo = 250
+        self.ultimo_tiro = pygame.time.get_ticks()
 
     def update(self):
-        self.speedx = 0
-        self.speedy = 0
+        self.veloc_y = 0
         keystate = pygame.key.get_pressed()
-        self.speedx = 9
         if keystate[pygame.K_UP]:
-            self.speedy = -9
+            self.veloc_y = -9
         if keystate[pygame.K_DOWN]:
-            self.speedy = 9
-        self.rect.y += self.speedy
+            self.veloc_y = 9
+        if keystate[pygame.K_SPACE]:
+            self.atirar()
+        self.rect.y += self.veloc_y
         if self.rect.top < 0:
             self.rect.top = 0
         if self.rect.bottom > ALTURA:
             self.rect.bottom = ALTURA
+
+    def atirar(self):
+        agora = pygame.time.get_ticks()
+        if agora - self.ultimo_tiro > self.tiro_intervalo:
+            self.ultimo_tiro = agora
+            tiro = Tiro(self.rect.centery, self.rect.right)
+            game_sprites.add(tiro)
+            tiros.add(tiro)
 
 class Boss(pygame.sprite.Sprite):
     def __init__(self):
@@ -51,17 +62,58 @@ class Boss(pygame.sprite.Sprite):
         self.rect.centery = ALTURA / 2
         self.rect.right = LARGURA - 10
         self.veloc_y = 7
+        self.tiro_intervalo = 800
+        self.ultimo_tiro = pygame.time.get_ticks()
+
     def update(self):
         self.rect.y += self.veloc_y
+        self.atirar()
         if self.rect.bottom > ALTURA:
             self.veloc_y = -9
         if self.rect.top < 0:
             self.veloc_y = 9
+    def atirar(self):
+        agora = pygame.time.get_ticks()
+        if agora - self.ultimo_tiro > self.tiro_intervalo:
+            self.ultimo_tiro = agora
+            tiroinimigo = Tiroinimigo(self.rect.centery, self.rect.left)
+            game_sprites.add(tiroinimigo)
+            tirosinimigo.add(tiroinimigo)
+
+class Tiro(pygame.sprite.Sprite):
+    def __init__(self, y, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((20, 10))
+        self.image.fill(VERDE)
+        self.rect = self.image.get_rect()
+        self.rect.left =  x
+        self.rect.centery = y
+        self.speedx = 10
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.left > LARGURA:
+            self.kill()
+
+class Tiroinimigo(pygame.sprite.Sprite):
+    def __init__(self, y, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((20, 10))
+        self.image.fill(VERMELHO)
+        self.rect = self.image.get_rect()
+        self.rect.right =  x
+        self.rect.centery = y
+        self.speedx = -10
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.right < 0:
+            self.kill()
 
 game_sprites = pygame.sprite.Group()
-player = Player()
+tiros = pygame.sprite.Group()
+tirosinimigo = pygame.sprite.Group()
+jogador = Jogador()
 boss = Boss()
-game_sprites.add(player)
+game_sprites.add(jogador)
 game_sprites.add(boss)
 
 # Game Loop
@@ -75,10 +127,9 @@ while rodando:
         # verifica se a janela foi fechada
         if event.type == pygame.QUIT:
             rodando = False
-
+            
     # Update
     game_sprites.update()
-
     # Render / Draw
     tela.fill(PRETO)
     game_sprites.draw(tela)
